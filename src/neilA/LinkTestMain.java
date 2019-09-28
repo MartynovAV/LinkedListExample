2a11e3ddaa28557050cc7006561bc22f102b88a3
MartynovAV <mcae@yandex.ru>
2019-09-28 13:08:48 +0300

    Initial, two examples of linked lists

---
+++ src/neilA/LinkTestMain.java
@@ -0,0 +1,18 @@
+package neilA;
+
+public class LinkTestMain {
+    public static void main(String[] args) {
+        MyLinkedList myList=new MyLinkedList();
+        myList.add(1);
+        myList.add(2);
+        myList.add(3);
+        myList.add(10);
+
+        System.out.println(myList);
+        System.out.println(myList.get(3));
+        myList.remove(2);
+        System.out.println(myList);
+        myList.remove(0);
+        System.out.println(myList);
+    }
+}
