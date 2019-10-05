e86c5a62e60592b9290fb1753ce061a909e8a072
MartynovAV <mcae@yandex.ru>
2019-10-06 00:07:55 +0300

    Compare two linked lists

--- src/mcaeList/MLinkedList.java
+++ src/mcaeList/MLinkedList.java
@@ -59,9 +59,9 @@ public class MLinkedList {
         }
         return Arrays.toString(arr);
     }
+
     //just printing
-    public void printList()
-    {
+    public void printList() {
         Node node = head;
         while (node != null) {
             System.out.print(node.getValue() + " ");
@@ -81,6 +81,7 @@ public class MLinkedList {
         this.head = head;
         size++;
     }
+
     // Reverse a linked list
     public void reverseListArr() {
         if (head == null) {
@@ -100,51 +101,118 @@ public class MLinkedList {
             temp = temp.getNext();
         }
     }
+
     //Iterative approach
 //https://www.geeksforgeeks.org/reverse-a-linked-list/
 //https://youtu.be/jY-EUKXYT20
-        public void reverseListIterat(){
-            head=reverseListIter();
-        }
-
-        private Node reverseListIter() {
-            if (head == null) {
-                return null;
-            }
-            Node prev=null;
-            Node current=head;
-            Node fwd=null;
-            while (current!=null){
-                fwd= current.getNext();
-                current.setNext(prev);
-                prev = current;
-                current = fwd;
-            }
-                head=prev;
-                return head;
-            }
-
-//Reverse a Linked List: Recursive | Java
-//https://www.youtube.com/watch?v=Ip4y7Inx7QY
-    public void reverseListRecursion(){
-        head=reverseListRec(this.head);
+    public void reverseListIterat() {
+        head = reverseListIter();
     }
-    private Node reverseListRec(Node curr) {
-        if (curr == null){
+
+    private Node reverseListIter() {
+        if (head == null) {
             return null;
-        } else if(curr.getNext()==null){
+        }
+        Node prev = null;
+        Node current = head;
+        Node fwd = null;
+        while (current != null) {
+            fwd = current.getNext();
+            current.setNext(prev);
+            prev = current;
+            current = fwd;
+        }
+        head = prev;
+        return head;
+    }
+
+    //Reverse a Linked List: Recursive | Java
+//https://www.youtube.com/watch?v=Ip4y7Inx7QY
+    public void reverseListRecursion() {
+        head = reverseListRec(this.head);
+    }
+
+    private Node reverseListRec(Node curr) {
+        if (curr == null) {
+            return null;
+        } else if (curr.getNext() == null) {
             return curr;
-        } else{
-            Node nextNode=curr.getNext();
+        } else {
+            Node nextNode = curr.getNext();
             curr.setNext(null);
-            Node rest=reverseListRec(nextNode);
+            Node rest = reverseListRec(nextNode);
             nextNode.setNext(curr);
             return rest;
         }
     }
+    //Compare to another list
+    public static boolean compareLists(MLinkedList list1, MLinkedList list2) {
+        boolean result = false;
+        Node node1 = list1.head;
+        Node node2 = list2.head;
+        int q1 = 0, q2 = 0;
+        if (node1 == null && node2 == null) {
+            return true;
+        }
+        while (node1 != null) {
+            node1 = node1.getNext();
+            q1++;
+        }
+        while (node2 != null) {
+            node2 = node2.getNext();
+            q2++;
+        }
 
+        if (q1 != q2) {
+            return false;
+        } else {
+            node1 = list1.head;
+            node2 = list2.head;
+            while (node1 != null) {
+                if (node1.getValue() == node2.getValue()) {
+                    result = true;
+                } else {
+                    result = false;
+                    break;
+                }
+                node1 = node1.getNext();
+                node2 = node2.getNext();
+            }
+            return result;
+        }
+    }
+
+    public static void compareTest(MLinkedList list1, MLinkedList list2) {
+        Node node1 = list1.head;
+        Node node2 = list2.head;
+
+        while (node1 != null) {
+            System.out.println(node1.getValue()==node2.getValue());
+            node1 = node1.getNext();
+            node2 = node2.getNext();
+
+        }
+    }
+    //GeeksForGeeks
+    public static boolean compareListsNew (MLinkedList list1, MLinkedList list2){
+        Node node1 = list1.head;
+        Node node2 = list2.head;
+
+        while (node1 != null&&node2 != null){
+            if(node1.getValue()!=node2.getValue()){
+                return false;}
+                /* If we reach here, then a and b are not null
+               and their data is same, so move to next nodes
+               in both lists */
+                node1 = node1.getNext();
+                node2 = node2.getNext();
+        }
+        // If linked lists are identical, then 'node1' and 'node2' must
+        // be null at this point.
+        return (node1 == null && node2== null);
     }
 
 
+}
 
 
--- src/mcaeList/MainTest.java
+++ src/mcaeList/MainTest.java
@@ -10,13 +10,26 @@ public class MainTest {
         list.addTo(2);
         list.addTo(8);
         list.addTo(11);
-        System.out.println(list);
+        //System.out.println(list);
 
-        list.insertHead(20);
-        System.out.println(list);
+        //list.insertHead(20);
+        //System.out.println(list);
 
-        list.reverseListIterat();
+        //list.reverseListIterat();
+        //System.out.println(list);
+
+        MLinkedList list2=new MLinkedList();
+        list2.addTo(1);
+        list2.addTo(2);
+        list2.addTo(8);
+        //list2.addTo(11);
+
         System.out.println(list);
+        System.out.println(list2);
+        System.out.println(MLinkedList.compareLists(list,list2));
+        System.out.println(MLinkedList.compareListsNew(list,list2));
+        //MLinkedList.compareTest(list,list2);
+
 
     }
 }
